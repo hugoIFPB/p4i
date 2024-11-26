854ef29dd2fbfd9a4975b0c4173158b9a868c881
Hugo Feitosa de Figueirêdo <hugo.figueiredo@ifpb.edu.br>
2024-11-26 08:09:58 -0300

    adicionando página de Aluno

---
+++ src/app/aluno/page.js
@@ -0,0 +1,8 @@
+import db from "@/lib/db"
+export default async () => {
+    const alunos = await db.query("select * from aluno")
+ return (<>
+    <h1>Lista de alunos</h1>
+    <div>{alunos.rows.map( a => (<div>{a.name} faz parte do projeto {a.project}</div>) )}</div>
+ </>);
+}
